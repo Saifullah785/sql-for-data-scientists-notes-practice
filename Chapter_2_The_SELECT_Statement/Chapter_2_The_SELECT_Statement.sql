70b76272eb052f2d313e804fd015645c157a27a5
Saif Ullah <muhammadalisaif785@gmail.com>
2025-08-06 09:29:08 +0500

    created using MySQL Workbench
    while practicing the SQL queries on The ORDER BY Clause: Sorting Results

--- Chapter_2_The_SELECT_Statement/Chapter_2_The_SELECT_Statement.sql
+++ Chapter_2_The_SELECT_Statement/Chapter_2_The_SELECT_Statement.sql
@@ -22,9 +22,31 @@ SELECT market_date, vendor_id, booth_number
 FROM farmers_market.vendor_booth_assignments
 LIMIT 5
 
+-- ==========The ORDER BY Clause: Sorting Results
+/*
+The ORDER BY clause is used to sort the output rows. In it, you list the columns
+you want to sort the results by, in order, separated by commas. You can also
+specify whether you want the sorting to be in ascending (ASC) or descending
+(DESC) order. ASC sorts text alphabetically and numeric values from low to high,
+and DESC sorts them in the reverse order. In MySQL, NULL values appear first
+when sorting in default ascending order.
+*/
+
+SELECT product_id, product_name
+FROM farmers_market.product
+ORDER BY product_name
+LIMIT 5
 
 
+SELECT product_id, product_name
+FROM farmers_market.product
+ORDER BY product_id DESC
+LIMIT 6
 
 
+SELECT market_date, vendor_id, booth_number
+FROM farmers_market.vendor_booth_assignments
+ORDER BY market_date, vendor_id
+LIMIT 5
 
 
