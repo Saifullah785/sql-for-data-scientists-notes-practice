35e066c82cfbe2dd45ad687d4f8689235c0c2989
Saif Ullah <muhammadalisaif785@gmail.com>
2025-08-09 08:03:34 +0500

    Created Using MySQL Workbench
    While working on practicing queries on Chapter 3, the where clause in the select statement

---
+++ Chapter_3_The_WHERE_Clause/Chapter_3_The_WHERE_Clause.sql
@@ -0,0 +1,25 @@
+-- ================Filtering SELECT Statement Results=================
+
+SELECT 
+	product_id,
+	product_name,
+	product_category_id
+FROM farmers_market.product
+WHERE 
+	product_category_id = 1
+LIMIT 5
+
+
+
+SELECT 
+	market_date,
+    customer_id, 
+    vendor_id,
+    product_id,
+    quantity,
+    quantity * cost_to_customer_per_qty AS price
+    
+FROM farmers_market.customer_purchases
+WHERE customer_id = 4
+ORDER BY market_date, vendor_id, product_id
+LIMIT 5
