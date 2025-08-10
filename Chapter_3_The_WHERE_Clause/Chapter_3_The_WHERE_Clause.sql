0d16956e35c8d7f6bdd6358dc1386891e4b57431
Saif Ullah <muhammadalisaif785@gmail.com>
2025-08-10 07:58:56 +0500

    Created Using MySQL Workbench
    while practicing the SQL queries on Filtering on Multiple Conditions

--- Chapter_3_The_WHERE_Clause/Chapter_3_The_WHERE_Clause.sql
+++ Chapter_3_The_WHERE_Clause/Chapter_3_The_WHERE_Clause.sql
@@ -23,3 +23,45 @@ FROM farmers_market.customer_purchases
 WHERE customer_id = 4
 ORDER BY market_date, vendor_id, product_id
 LIMIT 5
+
+-- ======================Filtering on Multiple Conditions===============
+
+SELECT 
+	market_date,
+    customer_id,
+    vendor_id,
+    product_id,
+    quantity,
+    quantity * cost_to_customer_per_qty AS price
+FROM farmers_market.customer_purchases
+WHERE customer_id = 3
+	OR customer_id = 4
+ORDER BY market_date, customer_id, vendor_id, product_id
+
+
+
+SELECT 
+	market_date,
+    customer_id,
+    vendor_id,
+    product_id,
+    quantity,
+    quantity * cost_to_customer_per_qty AS price
+FROM farmers_market.customer_purchases
+WHERE customer_id > 3
+	AND customer_id <= 5
+ORDER BY market_date, customer_id, vendor_id, product_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
