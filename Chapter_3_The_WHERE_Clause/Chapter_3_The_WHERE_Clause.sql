a2f813d45fe1808a9b9fb0c315a3cb6dbc9c86ef
Saif Ullah <muhammadalisaif785@gmail.com>
2025-08-11 08:44:22 +0500

    Created Using MySQL Workbench
    while practicing SQL queries on Multi-Column Conditional Filtering in chapter 3, the WHERE Clause

--- Chapter_3_The_WHERE_Clause/Chapter_3_The_WHERE_Clause.sql
+++ Chapter_3_The_WHERE_Clause/Chapter_3_The_WHERE_Clause.sql
@@ -53,10 +53,38 @@ WHERE customer_id > 3
 ORDER BY market_date, customer_id, vendor_id, product_id
 
 
+SELECT 
+	product_id,
+    product_name
+FROM farmers_market.product
+WHERE
+	product_id = 10
+    OR (product_id > 3
+    AND product_id < 8)
+    
+    
+
+SELECT 
+	product_id,
+    product_name
+FROM farmers_market.product
+WHERE 
+	(product_id = 10
+    OR product_id > 3)
+    AND product_id < 8
 
 
+-- =======================Multi-Column Conditional Filtering===============
 
-
+SELECT 
+	market_date,
+    customer_id,
+    vendor_id,
+    quantity * cost_to_customer_per_qty AS price
+FROM farmers_market.customer_purchases
+WHERE
+	customer_id = 4
+    AND vendor_id = 7
 
 
 
