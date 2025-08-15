845b35e496c10d55b98fe8c17f264c1ce9b555fc
Saif Ullah <muhammadalisaif785@gmail.com>
2025-08-15 08:48:24 +0500

    Created Using MySQL Workbench
    While studying and practicing queries on Chapter 03, the WHERE Clause

--- Chapter_3_The_WHERE_Clause/Chapter_3_The_WHERE_Clause.sql
+++ Chapter_3_The_WHERE_Clause/Chapter_3_The_WHERE_Clause.sql
@@ -235,3 +235,50 @@ WHERE
     AND vendor_id = 7
     
 - --------------------------------------------------------
+
+  -- =====================Filtering Using Subqueries======================= 
+  
+ SELECT * 
+ FROM farmers_market.market_date_info
+  
+  
+SELECT market_date, market_rain_flag
+FROM farmers_market.market_date_info
+WHERE market_rain_flag = 1
+
+
+- --------------------------------------------------------
+SELECT 
+	market_date,
+    customer_id,
+    vendor_id,
+    quantity * cost_to_customer_per_qty price
+FROM farmers_market.customer_purchases
+WHERE 
+	market_date IN
+		(
+        SELECT market_date
+        FROM farmers_market.market_date_info
+        WHERE market_rain_flag = 1
+        )
+LIMIT 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
