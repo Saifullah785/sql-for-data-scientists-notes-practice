7522ae2473c815fcbbc7eff62ed43879682e1c55
Saif Ullah <muhammadalisaif785@gmail.com>
2025-08-21 08:42:36 +0500

    Created Using MySQL Workbench
    while practicing the queries on Chapter_5_SQL_JOINs

--- Chapter_5_SQL_JOINs/Chapter_5_SQL_JOINs.sql
+++ Chapter_5_SQL_JOINs/Chapter_5_SQL_JOINs.sql
@@ -22,3 +22,41 @@ FROM product
  LEFT JOIN product_category
  ON product.product_category_id = product_category.
  product_category_id
+ 
+ 
+ -- =================================================================== 
+ 
+SELECT
+ p.product_id,
+ p.product_name,
+ pc.product_category_id,
+ pc.product_category_name
+FROM product AS p
+ LEFT JOIN product_category AS pc
+ ON p.product_category_id = pc.product_category_id
+ORDER BY pc.product_category_name, p.product_name
+ 
+  -- =================================================================== 
+ 
+ SELECT *
+ FROM customer AS c
+ LEFT JOIN customer_purchases AS cp
+	ON c.customer_id = cp.customer_id
+ 
+   -- =================================================================== 
+   
+SELECT c.*
+FROM customer AS c
+LEFT JOIN customer_purchases AS cp
+	ON c.customer_id = cp.customer_id
+WHERE cp.customer_id IS NULL
+
+
+SELECT *
+FROM customer AS c 
+RIGHT JOIN customer_purchases AS cp
+	ON c.customer_id = cp.customer_id
+ 
+ 
+ 
+ 
