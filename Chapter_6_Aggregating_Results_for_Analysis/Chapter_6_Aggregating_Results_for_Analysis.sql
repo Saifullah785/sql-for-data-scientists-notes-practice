0ba80f614539e4c60f216cd003ede646ac972d72
Saif Ullah <muhammadalisaif785@gmail.com>
2025-08-25 07:22:51 +0500

    created using MySQL workbench
    while practicing the queries on Displaying Group Summaries in Chapter_6_Aggregating_Results_for_Analysis

--- Chapter_6_Aggregating_Results_for_Analysis/Chapter_6_Aggregating_Results_for_Analysis.sql
+++ Chapter_6_Aggregating_Results_for_Analysis/Chapter_6_Aggregating_Results_for_Analysis.sql
@@ -19,3 +19,58 @@ GROUP BY market_date, customer_id
 ORDER BY market_date, customer_id
 
 -- ====================Displaying Group Summaries=============================
+
+SELECT 
+	market_date,
+    customer_id,
+    COUNT(*) AS items_purchassed
+FROM data_science.customer_purchases
+GROUP BY market_date, customer_id
+ORDER BY market_date, customer_id
+LIMIT 10
+
+-- ===================================================================
+
+
+SELECT * 
+FROM data_science.customer_purchases
+
+SELECT 
+	market_date,
+    customer_id,
+    SUM(Quantity) AS items_purchased
+FROM data_science.customer_purchases
+GROUP BY market_date, customer_id
+ORDER BY market_date, customer_id
+LIMIT 10
+
+-- ===================================================================
+
+SELECT 
+	market_date,
+    customer_id,
+    COUNT(DISTINCT product_id) AS different_products_purchased
+    FROM data_science.customer_purchases c
+    GROUP BY market_date, customer_id
+    ORDER BY market_date, customer_id
+    LIMIT 10
+
+-- ===================================================================
+
+SELECT 
+	market_date,
+    customer_id,
+    SUM(quantity) AS items_purchasesd,
+    COUNT(DISTINCT product_id) AS different_products_purchased
+FROM data_science.customer_purchases
+GROUP BY market_date, customer_id
+ORDER BY market_date, customer_id
+LIMIT 10
+
+
+
+
+
+
+
+
