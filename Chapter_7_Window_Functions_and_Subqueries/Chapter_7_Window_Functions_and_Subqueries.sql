1beffe3de533e199d9de5b9e8c8c76247f069933
Saif Ullah <muhammadalisaif785@gmail.com>
2025-09-08 07:39:57 +0500

    created using MySQL Workbench
    while practicing the queries on ROW NUMBER in the Chapter_7_Window_Functions_and_Subqueries

---
+++ Chapter_7_Window_Functions_and_Subqueries/Chapter_7_Window_Functions_and_Subqueries.sql
@@ -0,0 +1,28 @@
+
+-- ===========Chapter 7 Window Functions and Subqueries=======================
+
+-- ===========ROW NUMBER=======================
+
+SELECT 
+	vendor_id,
+    MAX(original_price) AS highest_price
+FROM data_science.vendor_booth_inventory
+GROUP BY vendor_id
+ORDER BY vendor_id
+
+-- ===================================================================
+
+SELECT 
+	vendor_id,
+    market_date,
+    product_id,
+    original_price,
+    ROW_NUMBER() OVER (PARTITION BY vendor_id ORDER BY original_price DESC) AS
+price_rank
+FROM data_science.vendor_booth_inventory ORDER BY vendor_id, original_price DESC
+
+
+
+
+
+
