b0684b5985bbebe93767fd2cac1aa0fcfa2b676b
Saif Ullah <muhammadalisaif785@gmail.com>
2025-09-09 07:27:51 +0500

    created using MySQL Workbench

--- Chapter_7_Window_Functions_and_Subqueries/Chapter_7_Window_Functions_and_Subqueries.sql
+++ Chapter_7_Window_Functions_and_Subqueries/Chapter_7_Window_Functions_and_Subqueries.sql
@@ -21,7 +21,20 @@ SELECT
 price_rank
 FROM data_science.vendor_booth_inventory ORDER BY vendor_id, original_price DESC
 
+-- ===================================================================
 
+SELECT * FROM
+(
+	SELECT 
+		vendor_id,
+        market_date,
+		product_id,
+        original_price,
+        ROW_NUMBER() OVER (PARTITION BY vendor_id ORDER BY original_price DESC) AS
+price_rank
+	FROM data_science.vendor_booth_inventory
+    ORDER BY vendor_id) x
+WHERE x.price_rank = 2
 
 
 
