f52c4651207d41ab6ee9a7dcbe5de23199696a70
Saif Ullah <muhammadalisaif785@gmail.com>
2025-09-11 07:13:06 +0500

    created using MySQL Workbench
    while practicing the queries on NTILE in the Chapter_7_Window_Functions_and_Subqueries

--- Chapter_7_Window_Functions_and_Subqueries/Chapter_7_Window_Functions_and_Subqueries.sql
+++ Chapter_7_Window_Functions_and_Subqueries/Chapter_7_Window_Functions_and_Subqueries.sql
@@ -50,10 +50,17 @@ price_rank
 ORDER BY vendor_id, original_price DESC
 
 
+-- ===========NTILE=======================
 
 
-
-
+SELECT 
+	vendor_id,
+    market_date,
+    product_id,
+    original_price,
+    NTILE(10) OVER ( ORDER BY original_price DESC) AS price_ntile
+    FROM data_science.vendor_booth_inventory
+ORDER BY original_price DESC
 
 
 
