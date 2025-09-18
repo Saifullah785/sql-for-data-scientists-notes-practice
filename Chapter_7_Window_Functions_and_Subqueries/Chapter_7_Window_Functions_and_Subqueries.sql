316258f53bd9e9d0979998e1e5f3ed90b924bb44
Saif Ullah <muhammadalisaif785@gmail.com>
2025-09-18 07:48:12 +0500

    created using MySQL Workbench
    while practicing the queries on LAG and LEAD in the Chapter_7_Window_Functions_and_Subqueries

--- Chapter_7_Window_Functions_and_Subqueries/Chapter_7_Window_Functions_and_Subqueries.sql
+++ Chapter_7_Window_Functions_and_Subqueries/Chapter_7_Window_Functions_and_Subqueries.sql
@@ -156,10 +156,25 @@ SELECT
     ORDER BY market_date, vendor_id, booth_number
 
 
+-- ===================================================================
+
+SELECT * FROM 
+(
+	SELECT 
+		market_date,
+        vendor_id,
+        booth_number,
+        LAG(booth_number,1) OVER (PARTITION BY vendor_id ORDER BY market_date, vendor_id)
+        AS previous_booth_number
+        FROM data_science.vendor_booth_assignments
+        ORDER BY market_date, vendor_id, booth_number
+        
+) x
+WHERE x.market_date = '2019-04-10'
+		AND (x.booth_number <> x.previous_booth_number OR x.previous_booth_number is NULL)
 
 
-
-
+-- ===================================================================
 
 
 
