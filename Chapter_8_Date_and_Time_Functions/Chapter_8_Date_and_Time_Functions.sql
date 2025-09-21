33e30fdca13ca5d0d1c7ca43251793ef48440485
Saif Ullah <muhammadalisaif785@gmail.com>
2025-09-21 08:46:51 +0500

    created using MySQL Workbench
    while practicing the queries on EXTRACT and DATE_PART in the Chapter_8_Date_and_Time_Functions

--- Chapter_8_Date_and_Time_Functions/Chapter_8_Date_and_Time_Functions.sql
+++ Chapter_8_Date_and_Time_Functions/Chapter_8_Date_and_Time_Functions.sql
@@ -1,4 +1,8 @@
 
+
+
+-- =========Setting datetime Field Values=======================
+
 CREATE TABLE data_science.datetime_demo AS
 (
 	SELECT market_date,
@@ -13,3 +17,16 @@ CREATE TABLE data_science.datetime_demo AS
 			AS market_end_datetime
 		FROM data_science.market_date_info        
 )
+
+-- ==========EXTRACT and DATE_PART=======================
+    
+SELECT market_start_datetime,
+	EXTRACT(DAY FROM market_start_datetime) AS mksrt_day,
+    EXTRACT(MONTH FROM market_start_datetime) AS mktsrt_month,
+    EXTRACT(YEAR FROM market_Start_datetime) AS mktsrt_year,
+    EXTRACT(HOUR FROM market_start_datetime) AS mktsrt_hour,
+    EXTRACT(MINUTE FROM market_start_datetime) AS mktsrt_minute
+FROM data_science.datetime_demo
+WHERE market_Start_datetime = '2019-03-02 08:00:00'
+
+-- ===================================================================
