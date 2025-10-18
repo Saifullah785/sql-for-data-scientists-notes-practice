e0a6da065052f6f42b8a7a2b0343a3b97d85d295
Saif Ullah <muhammadalisaif785@gmail.com>
2025-10-18 11:10:49 +0500

    created using the MySQL Workbench
    while practicing the queries and completing the Exploring Inventory vs. Sales, and also completing the Chapter_09_Exploratory_Data_Analysis_with_SQL

--- Chapter_09_Exploratory_Data_Analysis_with_SQL/Chapter_09_Exploratory_Data_Analysis_with_SQL.sql
+++ Chapter_09_Exploratory_Data_Analysis_with_SQL/Chapter_09_Exploratory_Data_Analysis_with_SQL.sql
@@ -127,12 +127,36 @@ LIMIT 10
 
 -- ===================================================================
 
-
-
-
-
-
-
+SELECT vi.market_date,
+	vi.vendor_id,
+    v.vendor_name,
+    vi.product_id,
+    p.product_name,
+    vi.quantity AS quantity_available,
+    sales.quantity_sold,
+    vi.original_price,
+    sales.total_sales
+FROM farmers_market.vendor_booth_inventory AS vi
+	LEFT JOIN
+		(
+        SELECT market_date,
+			vendor_id,
+            product_id,
+            SUM(quantity) AS quantity_sold,
+            SUM(quantity * cost_to_customer_per_qty) AS total_sales
+		FROM farmers_market.customer_purchases
+        GROUP BY market_date, vendor_id, product_id
+        ) AS sales
+	ON vi.market_date = sales.market_date
+		AND vi.vendor_id = sales.vendor_id
+        AND vi.product_id = sales.product_id
+	LEFT JOIN farmers_market.vendor v
+		ON vi.vendor_id = v.vendor_id
+	LEFT JOIN farmers_market.product p 
+		ON vi.product_id = p.product_id
+WHERE vi.vendor_id = 7
+	AND vi.product_id = 4
+ORDER BY vi.market_date, vi.vendor_id, vi.product_id
 
 
 
