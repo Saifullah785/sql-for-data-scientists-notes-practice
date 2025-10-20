b4b78a5e5712c6899f6407c26adf933c076b6d06
Saif Ullah <muhammadalisaif785@gmail.com>
2025-10-20 10:30:07 +0500

    created using MySQL Workbench
    while practicing the queries on Thinking Through Analytical Dataset Requirements in the Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting

--- Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting/Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting.sql
+++ Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting/Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting.sql
@@ -1,4 +1,4 @@
--- ==============Chapter 10 ■ Building SQL Datasets for Analytical Reporting==========
+-- ==============Chapter 10 Building SQL Datasets for Analytical Reporting==========
 
 -- ===========Thinking Through Analytical Dataset Requirements===============
 
@@ -7,3 +7,13 @@ SELECT
     vendor_id,
     quantity * cost_to_customer_per_qty
 FROM farmers_market.customer_purchases
+
+-- ====================================================================
+
+SELECT
+	market_date,
+    vendor_id,
+    ROUND(SUM(quantity * cost_to_customer_per_qty),2) AS sales
+FROM farmers_market.customer_purchases
+GROUP BY market_date, vendor_id
+ORDER BY market_date, vendor_id
