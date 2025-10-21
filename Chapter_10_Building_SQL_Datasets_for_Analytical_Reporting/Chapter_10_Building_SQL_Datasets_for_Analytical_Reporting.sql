d5a905711a03283449e98505be447d9f12797dc9
Saif Ullah <muhammadalisaif785@gmail.com>
2025-10-21 10:37:24 +0500

    created using MySQL Workbench
    while practicing the queries on Thinking Through Analytical Dataset Requirements in the Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting

--- Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting/Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting.sql
+++ Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting/Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting.sql
@@ -17,3 +17,56 @@ SELECT
 FROM farmers_market.customer_purchases
 GROUP BY market_date, vendor_id
 ORDER BY market_date, vendor_id
+
+-- ====================================================================
+SELECT
+	cp.market_date,
+    md.market_day,
+    md.market_week,
+    md.market_year,
+    cp.vendor_id,
+    v.vendor_name,
+    v.vendor_type,
+    ROUND(SUM(cp.quantity * cp.cost_to_customer_per_qty),2) AS sales
+FROM farmers_market.customer_purchases AS cp
+	LEFT JOIN farmers_market.market_date_info AS md
+		ON cp.market_date = md.market_date
+	LEFT JOIN farmers_market.vendor AS v
+		ON cp.vendor_id = v.vendor_id
+GROUP BY cp.market_date, cp.vendor_id
+ORDER BY cp.market_date, cp.vendor_id
+    
+    
+-- ====================================================================    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
