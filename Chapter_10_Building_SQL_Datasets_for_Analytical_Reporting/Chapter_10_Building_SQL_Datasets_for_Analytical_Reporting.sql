acaa3106fd496587432827a172bc991e654b42c0
Saif Ullah <muhammadalisaif785@gmail.com>
2025-10-22 11:00:39 +0500

    created using MySQL Workbench
    While practicing the queries on Using Custom Analytical Datasets in SQL: CTEs and
    Views in the Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting

--- Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting/Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting.sql
+++ Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting/Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting.sql
@@ -39,10 +39,34 @@ ORDER BY cp.market_date, cp.vendor_id
     
 -- ====================================================================    
     
-    
-    
-    
-    
+
+WITH sales_by_day_vendor AS
+	(
+		SELECT
+			cp.market_date,
+            md.market_day,
+            md.market_week,
+            md.market_year,
+            cp.vendor_id,
+            v.vendor_name,
+            v.vendor_type,
+            ROUND(SUM(cp.quantity * cp.cost_to_customer_per_qty),2) AS sales
+		FROM farmers_market.customer_purchases AS cp
+			LEFT JOIN farmers_market.market_date_info AS md
+				ON cp.market_date = md.market_date
+			LEFT JOIN farmers_market.vendor AS v
+				ON cp.vendor_id = v.vendor_id
+		GROUP BY cp.market_date, cp.vendor_id
+        ORDER BY cp.market_date, cp.vendor_id
+	)
+    SELECT
+		s.market_year,
+        s.market_week,
+        SUM(s.sales) AS weekly_sales
+	FROM sales_by_day_vendor AS s
+    GROUP BY s.market_year, s.market_week
+
+-- ==================================================================== 
     
     
     
