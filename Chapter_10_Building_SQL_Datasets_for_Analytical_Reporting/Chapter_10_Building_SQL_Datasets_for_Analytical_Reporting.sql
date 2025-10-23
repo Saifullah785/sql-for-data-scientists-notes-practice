376241d16f92a714b50b4fe872dda657b2142a15
Saif Ullah <muhammadalisaif785@gmail.com>
2025-10-23 09:44:03 +0500

    created using MySQL Workbench
    While learning and practicing the queries in the Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting

--- Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting/Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting.sql
+++ Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting/Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting.sql
@@ -67,7 +67,48 @@ WITH sales_by_day_vendor AS
     GROUP BY s.market_year, s.market_week
 
 -- ==================================================================== 
+CREATE VIEW farmers_market.vw_sales_by_day_vendor AS
+SELECT
+	cp.market_date,
+    md.market_day,
+    md.market_week,
+    md.market_year,
+    cp.vendor_id,
+    v.vendor_name,
+    v.vendor_type,
+    ROUND(SUM(cp.quantity * cp.cost_to_customer_per_qty),2) AS sales
     
+FROM farmers_market.customer_purchases AS cp
+	LEFT JOIN farmers_market.market_date_info AS md
+		ON cp.market_date = md.market_date
+	LEFT JOIN farmers_market.vendor AS v
+		ON cp.vendor_id = v.vendor_id
+GROUP BY cp.market_date, cp.vendor_id
+ORDER BY cp.market_date, cp.vendor_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     
     
