c4bc3644d06328a79a993ff5bc1b6c9703f9d68b
Saif Ullah <muhammadalisaif785@gmail.com>
2025-10-25 10:54:35 +0500

    created using MySQL Workbench
    While practicing the queries in Taking SQL Reporting Further in the Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting

--- Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting/Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting.sql
+++ Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting/Chapter_10_Building_SQL_Datasets_for_Analytical_Reporting.sql
@@ -95,7 +95,45 @@ WHERE s.market_date BETWEEN '2020-04-01' AND '2020-04-30'
 ORDER BY market_date
 
 
+-- ==================================================================== 
+CREATE VIEW farners_marktet.vw_sales_per_date_vendor_product AS
 
+SELECT
+	vi.market_date,
+    vi.vendor_id,
+	v.vendor_name,
+    vi.product_id,
+    p.product_name,
+    vi.quantity AS quantity_available,
+    sales.quantity_sold,
+    ROUND((sales.quantity_sold / vi.quantity) * 100, 2 )AS percent_of_available_sold,
+    vi.original_price,
+    (vi.original_price * sales.quantity_sold) - sales.total_sales AS 
+    discount_amount,
+    sales.total_sales
+    FROM farmers_market.vendor_booth_inventory AS vi
+		LEFT JOIN
+			(
+            SELECT market_date,
+				vendor_id,
+                product_id,
+                SUM(quantity) quantity_sold,
+                SUM(quantity * cost_to_customer_per_qty) AS total_sales
+			FROM farmers_market.customer_purchases
+            GROUP BY market_date, vendor_id, product_id
+            ) AS sales
+		ON vi.market_date = sales.market_date
+			AND vi.vendor_id = sales.vendor_id
+            AND vi.product_id = sales.product_id
+            
+		LEFT JOIN farmers_market.vendor v
+			ON vi.vendor_id = v.vendor_id
+			
+            LEFT JOIN farmers_market.product p
+				ON vi.product_id = p.product_id
+		ORDER BY vi.vendor_id, vi.product_id, vi.market_date
+            
+-- ====================================================================                     
 
 
 
