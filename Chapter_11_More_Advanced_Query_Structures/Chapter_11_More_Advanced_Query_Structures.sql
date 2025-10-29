9c8bab9649f27c74503163521cdb58dd2778a5a6
Saif Ullah <muhammadalisaif785@gmail.com>
2025-10-29 11:27:36 +0500

    created using MySQL Workbench
    while understanding and practicing the queries in the Chapter_11_More_Advanced_Query_Structures

--- Chapter_11_More_Advanced_Query_Structures/Chapter_11_More_Advanced_Query_Structures.sql
+++ Chapter_11_More_Advanced_Query_Structures/Chapter_11_More_Advanced_Query_Structures.sql
@@ -11,3 +11,50 @@ UNION
 SELECT market_year, MIN(market_Date) AS first_market_date
 FROM farmers_market.market_date_info
 WHERE  market_year = '2020'  
+
+-- ========================================================
+
+WITH
+product_quantity_by_date AS
+(
+	SELECT
+		vi.market_date,
+        vi.product_id,
+        p.product_name,
+        SUM(vi.quantity) AS total_quantity_available,
+        p.product_qty_type
+	FROM farmers_market.vendor_booth_inventory vi
+		lEFT JOIN farmers_market.product p
+			ON vi.product_id = p.product_id
+	GROUP BY market_date, product_id
+)
+SELECT * FROM 
+(
+	SELECT
+		
+        market_date,
+        product_id,
+        product_name,
+        total_quantity_available,
+        product_qty_type,
+        RANK() OVER (PARTITION BY market_date ORDER BY total_quantity_available DESC) AS quantity_rank
+        
+	FROM product_quantity_by_date
+    WHERE product_qty_type = 'unit'
+    
+    UNION
+    SELECT
+		market_date,
+        product_id,
+        product_name,
+        total_quantity_available,
+        product_qty_type,
+        RANK() OVER (PARTITION BY market_date ORDER BY total_quantity_available DESC) AS quantity_rank
+        FROM product_quantity_by_date
+        WHERE product_qty_type = 'lbs'
+        
+) x
+
+WHERE x.quantity_rank = 1
+ORDER BY market_date
+		
