ce5ea783f83c960bbbbaedf82ca74d2816e26eac
Saif Ullah <muhammadalisaif785@gmail.com>
2025-11-02 12:07:12 +0500

    created using MySQL WOrkbench
    while understanding and practicing the queries in the Chapter_11_More_Advanced_Query_Structures

--- Chapter_11_More_Advanced_Query_Structures/Chapter_11_More_Advanced_Query_Structures.sql
+++ Chapter_11_More_Advanced_Query_Structures/Chapter_11_More_Advanced_Query_Structures.sql
@@ -128,6 +128,34 @@ FROM sales_per_market_date AS cm
 		ON pm.market_date < cm.market_date
 WHERE cm.market_date = '2019-04-13'
 
+-- ====================================================================
+
+WITH 
+sales_per_market_date AS
+ (
+	SELECT
+		market_date,
+        ROUND(SUM(quantity * cost_to_customer_per_qty),2) AS sales
+	FROM farmers_market.customer_purchases
+    GROUP BY market_date
+    ORDER BY market_date
+)
+
+SELECT
+	cm.market_date,
+    cm.sales,
+    MAX(pm.sales) AS previous_max_sales
+FROM sales_per_market_date AS cm
+	LEFT JOIN sales_per_market_date AS pm
+		ON pm.market_date < cm.market_date
+WHERE cm.market_date = '2019-04-13'
+GROUP BY cm.market_date, cm.sales
+
+-- ====================================================================
+
+
+
+
 
 
 
