2d45fce1df82649f03fb84496624a037cf11539d
Saif Ullah <muhammadalisaif785@gmail.com>
2025-11-07 07:08:47 +0500

    created using MySQL Workbench
    while practicing the queries on Counting New vs. Returning Customers by Week in the Chapter_11_More_Advanced_Query_Structures

--- Chapter_11_More_Advanced_Query_Structures/Chapter_11_More_Advanced_Query_Structures.sql
+++ Chapter_11_More_Advanced_Query_Structures/Chapter_11_More_Advanced_Query_Structures.sql
@@ -220,10 +220,40 @@ COUNT(
 
 -- ====================================================================
 
+WITH 
+customer_markets_attended AS
+	(
+		SELECT DISTINCT
+			customer_id,
+            market_date,
+            MIN(market_date) OVER (PARTITION BY cp.customer_id) AS first_purchase_date
+		FROM farmers_market.customer_purchases cp
+	)
+    SELECT
+		md.market_year,
+        md.market_week,
+        COUNT(customer_id) AS customer_visit_count,
+        COUNT(DISTINCT customer_id) AS distinct_customer_count,
+        COUNT(DISTINCT
+			CASE WHEN cma.market_date = cma.first_purchase_date
+				THEN customer_id
+                ELSE NULL
+			END) AS new_customer_count,
+		COUNT(DISTINCT
+			CASE WHEN cma.market_date = cma.first_purchase_date
+				THEN customer_id
+                ELSE NULL
+			END)
+            / COUNT(DISTINCT customer_id)
+            AS new_customer_present
+	FROM customer_markets_attended AS cma
+		LEFT JOIN farmers_market.market_date_info AS md
+			ON cma.market_date = md.market_date
+	GROUP BY md.market_year, md.market_week
+    ORDER BY md.market_year, md.market_week
 
 
-
-
+-- ====================================================================
 
 
 
