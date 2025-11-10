71d5ea05fc7510075f082433c66d51dd016bac6b
Saif Ullah <muhammadalisaif785@gmail.com>
2025-11-10 18:27:27 +0500

    created using MySQL Wokbench
    while practicing the queries in the Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL.

--- Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL/Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL.sql
+++ Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL/Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL.sql
@@ -10,3 +10,22 @@ FROM farmers_market.customer_purchases AS cp
 		ON cp.market_date = md.market_date
 GROUP BY md.market_year, md.market_week
 ORDER BY md.market_year, md.market_week
+
+-- ===========================================================================================
+
+WITH
+customer_market_attended AS
+	(
+		SELECT DISTINCT
+			customer_id,
+            market_date,
+		FROM farmers_market.customer_purchases
+        ORDER BY customer_id, market_date
+	)
+    SELECT
+		cp.market_date,
+        cp.customer_id,
+        SUM(cp.quantity * cp.cost_to_customer_per_qty) AS purchase_total,
+        COUNT(DISTINCT cp.vendor_id) AS vendors_patronized,
+        COUNT(DISTINCT cp.product_id) AS different_product_purchases,
+        
