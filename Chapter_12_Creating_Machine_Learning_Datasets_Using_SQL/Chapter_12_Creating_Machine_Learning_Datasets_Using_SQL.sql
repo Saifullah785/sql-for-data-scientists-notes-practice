ce8cd7d751fc5a18994e26e3926b0571fca35054
Saif Ullah <muhammadalisaif785@gmail.com>
2025-11-11 10:41:50 +0500

    created using MySQL Workbench
    while practicing the queries in the Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL

--- Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL/Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL.sql
+++ Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL/Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL.sql
@@ -28,4 +28,45 @@ customer_market_attended AS
         SUM(cp.quantity * cp.cost_to_customer_per_qty) AS purchase_total,
         COUNT(DISTINCT cp.vendor_id) AS vendors_patronized,
         COUNT(DISTINCT cp.product_id) AS different_product_purchases,
+         (SELECT MIN(cma.market_date)
+		FROM customer_markets_attended AS cma
+	WHERE cma.customer_id = cp.customer_id
+		AND cma.market_date > cp.market_date
+	GROUP BY cma.customer_id) AS customer_next_market_date,
+    DATEDIFF(
+		(SELECT MIN(cma2.market_date)
+        FROM customer_markets_attended AS cma2
+        WHERE cma2.customer_id = cp.customer_id
+         AND cma2.market_date > cp.market_date
+		GROUP BY cma2.customer_id),
+	cp.market_date) AS days_until_customer_next_market_date,
+    CASE WHEN
+		DATEDIFF(
+			(SELECT MIN(cma3.market_date)
+            FROM customer_markets_attended AS cma3
+            WHERE cma3.customer_id = cp.customer_id
+				AND cma3.market_date > cp.market_date
+			GROUP BY cma3.customer_id),
+		cp.market_date) <=30
+	THEN 1
+    ELSE 0 END AS purchased_again_within_30_days
+FROM farmers_market.customer_purchases AS cp
+GROUP BY cp.customer_id, cp.market_date
+ORDER BY cp.customer_id, cp.market_date
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
