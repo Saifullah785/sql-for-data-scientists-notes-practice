c98236794bdc4c4c2d768ba09572870acf407f18
Saif Ullah <muhammadalisaif785@gmail.com>
2025-11-15 10:37:12 +0500

    created using MySQL Workbench
    while practicing the queries in the Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL

--- Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL/Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL.sql
+++ Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL/Chapter_12_Creating_Machine_Learning_Datasets_Using_SQL.sql
@@ -143,8 +143,64 @@ GROUP BY cp.customer_id, cp.market_date
 ORDER BY cp.customer_id, cp.market_date
 
  
- 
 
+   -- ================================== Feature Engineering ==================================
+WITH 
+customer_markets_attended AS
+	(
+		SELECT
+			customer_id,
+            market_date,
+            ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY market_date)
+	AS market_count
+		FROM farmers_market.customer_purchases
+        GROUP BY customer_id, market_date
+        ORDER BY customer_id, market_date
+	)
+    SELECT
+			cp.customer_id,
+            cp.market_date,
+            EXTRACT(MONTH FROM cp.market_date) AS market_month,
+            SUM(cp.quantity * cp.cost_to_customer_per_qty) AS purchase_total,
+            COUNT(DISTINCT cp.vendor_id) AS vendors_patronized,
+            MAX(CASE WHEN cp.vendor_id = 7 THEN 1 ELSE 0 END) purchased_from_vendor_7,
+            MAX(CASE WHEN cp.vendor_id = 8 THEN 1 ELSE 0 END) purchased_from_vendor_8,
+            COUNT(DISTINCT cp.product_id) AS different_products_purchased,
+            DATEDIFF(cp.market_date,
+				(SELECT MAX(cma.market_date)
+					FROM customer_markets_attended AS cma
+                    WHERE cma.customer_id = cp.customer_id
+						AND cma.market_date < cp.market_date
+					GROUP BY cma.customer_id)
+				) days_since_last_customer_market_date,
+			(SELECT MAX(market_count)
+				FROM customer_markets_attended as cma
+                WHERE cma.customer_id = cp.customer_id
+					AND cma.market_date < cp.market_date
+                    AND DATEDIFF(cp.market_date, cma.market_date) <=30) AS
+			attended_count,
+            (SELECT COUNT(market_date)
+				FROM customer_markets_attended AS cma
+                WHERE cma.customer_id = cp.customer_id
+					AND cma.market_date < cp.market_date
+                    AND DATEDIFF(cp.market_date, cma.market_date) <= 30) AS
+			customer_markets_attended_30days_count,
+				CASE WHEN
+					DATEDIFF(
+						(SELECT MIN(cma.market_date)
+							FROM customer_markets_attended AS cma
+                            WHERE cma.customer_id = cp.customer_id
+								AND cma.market_date > cp.market_date
+							GROUP BY cma.customer_id),
+					cp.market_date) <=30
+				THEN 1
+                ELSE 0
+                END AS purchases_again_within_30_days
+			FROM farmers_market.customer_purchases AS cp
+            GROUP BY cp.customer_id, cp.market_date
+            ORDER BY cp.customer_id, cp.market_date
+
+   -- ===========================================================================================
  
  
  
