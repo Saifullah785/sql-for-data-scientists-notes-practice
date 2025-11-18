90d15530fa3238899fe79ae0cd2daf5c2ff0891b
Saif Ullah <muhammadalisaif785@gmail.com>
2025-11-18 10:55:08 +0500

    created using MySQL Workbench
    For understanding and practicing the queries in Chapter 13: Analytical Dataset Development Examples

--- Chapter_13_Analytical_Dataset_Development_Examples/Chapter_13_Analytical_Dataset_Development_Examples.sql
+++ Chapter_13_Analytical_Dataset_Development_Examples/Chapter_13_Analytical_Dataset_Development_Examples.sql
@@ -39,9 +39,27 @@ FROM customer_purchases cp
 WHERE p.product_category_id = 1
 -- =============================================================================================
 
+SELECT 
+	mdi.market_date,
+    mdi.market_week,
+    mdi.market_year,
+    mdi.market_rain_flag,
+    mdi.market_snow_flag,
+    cp.market_date,
+    cp.customer_id,
+    cp.quantity,
+    cp.cost_to_customer_per_qty,
+    p.product_category_id
+    
+FROM customer_purchases cp
+	INNER JOIN product p
+		ON cp.product_id = p.product_id
+			AND p.product_category_id = 1
+	RIGHT JOIN market_date_info mdi
+		ON mdi.market_date = cp.market_date
 
 
-
+-- =============================================================================================
 
 
 
