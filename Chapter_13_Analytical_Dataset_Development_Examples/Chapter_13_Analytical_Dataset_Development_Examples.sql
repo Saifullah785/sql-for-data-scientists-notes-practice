2ff392262540af1a24a23bf47120dc4d01b994a0
Saif Ullah <muhammadalisaif785@gmail.com>
2025-12-04 10:22:19 +0500

    created using MySQL Workbench
    while practicing the queries in the Chapter_13_Analytical_Dataset_Development_Examples

--- Chapter_13_Analytical_Dataset_Development_Examples/Chapter_13_Analytical_Dataset_Development_Examples.sql
+++ Chapter_13_Analytical_Dataset_Development_Examples/Chapter_13_Analytical_Dataset_Development_Examples.sql
@@ -471,6 +471,59 @@ GROUP BY
     vi.original_price
     
 -- =============================================================================================
+USE farmers_market;
+WITH product_price AS
+(
+	SELECT
+		mdi.market_season,
+        mdi.market_year,
+        MIN(MONTH(vi.market_date)) OVER  (PARTITION BY market_season) AS
+	Month_market_season_sort,
+		vi.original_price,
+        NTILE(3) OVER (PARTITION BY market_year, market_season ORDER BY
+	original_price) AS price_ntile,
+		NTILE(3) OVER (PARTITION BY market_year, market_season ORDER BY
+	original_price DESC) AS price_ntile_desc,
+		COUNT(DISTINCT CONCAT(vi.product_id, vi.vendor_id)) AS product_count,
+        SUM(cp.quantity) AS quantity_sold,
+        SUM(cp.quantity * cp.cost_to_customer_per_qty) AS total_sales
+	FROM product AS p
+		LEFT JOIN vendor_booth_inventory AS vi
+			ON vi.product_id = p.product_id
+		LEFT JOIN market_date_info AS mdi
+			ON vi.market_date = mdi.market_date
+		LEFT JOIN customer_purchases AS cp
+			ON vi.product_id = cp.product_id 
+            AND vi.vendor_id = cp.vendor_id
+            AND vi.market_date = cp.market_date
+		WHERE market_year IS NOT NULL 
+		GROUP BY 
+			mdi.market_year,
+            mdi.market_season,
+            vi.original_price
+		)
+    
+-- =============================================================================================
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
