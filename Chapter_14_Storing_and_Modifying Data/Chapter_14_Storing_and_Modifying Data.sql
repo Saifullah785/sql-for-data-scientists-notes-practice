dc9c819fd4dd5373cfa600231b23372c32948f4c
Saif Ullah <muhammadalisaif785@gmail.com>
2025-12-07 10:28:14 +0500

    created using MySQL Workbench
    while practicing the queries in the Chapter_14_Storing_and_Modifying Data

--- Chapter_14_Storing_and_Modifying Data/Chapter_14_Storing_and_Modifying Data.sql
+++ Chapter_14_Storing_and_Modifying Data/Chapter_14_Storing_and_Modifying Data.sql
@@ -11,3 +11,53 @@ SELECT * FROM farmers_market.product_units
 ;
 DROP TABLE farmers_market.product_units
 ;
+
+-- =====================================================================================
+CREATE VIEW farmers_market.product_units_vw AS
+(
+	SELECT *
+    FROM farmers_market.product
+    WHERE product_qty_type = "unit"
+    
+)
+;
+
+SELECT * FROM farmers_market.product_units_vw
+;
+
+DROP VIEW farmers_market.product_units_vw
+;
+
+-- =====================================================================================
+
+
+CREATE TABLE farmers_market.product_units AS
+(
+		SELECT p. *,
+			CURRENT_TIMESTAMP AS snapshot_timestamp
+		FROM farmers_market.product AS p
+        WHERE product_qty_type = "unit"
+)
+
+
+-- =====================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
